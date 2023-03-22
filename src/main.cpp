ca57c6173f69ed291181ef2af4887a0c8a1a4de2
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-03-22 21:50:01 +0100

    Improve vk debug logger

--- src/main.cpp
+++ src/main.cpp
@@ -316,7 +316,10 @@ private:
 				  VkDebugUtilsMessageTypeFlagsEXT messageType,
 				  const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
 				  void* pUserData) {
-		std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+		if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
+			std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+		else
+			std::cout << "validation layer: " << pCallbackData->pMessage << std::endl;
 
 		return VK_FALSE;
 	}
